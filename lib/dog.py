2574d88ea3fba0d3c69f19a8dc603a7aa94c7675
ShernOC <sherlynea1994@hotmail.com>
2024-09-10 17:02:45 +0300

    added methods instances

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,20 @@
 
 class Dog:
     # Class body goes here
+    
+        
 
     #Instance method definition
-    pass
+    def bark(self): 
+        print("Woof!")
+        
+    def sit(self): 
+        print("The dog is sitting.")
+        
+        
+fido = Dog() # first instance 
+fido.bark()
+snoopy = Dog()
+snoopy.bark() # sencond instance 
+
+
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,17 @@
 
 class Person:
     # Class body goes here
-
     #Instance method definition
-    pass
+    def talk(self): # method 1. 
+        print("Hello World!")
+        
+    def walk(self): # method 2 
+        print("The person is walking.")
+        
+# To call the class we have to have: 
+# An instance is created where the object is created "hello" from the Person class. The object (hello) can now use the method talk, defined in class. 
+hello = Person()
+
+#Method is called. 
+hello.talk()
+hello.walk() 
